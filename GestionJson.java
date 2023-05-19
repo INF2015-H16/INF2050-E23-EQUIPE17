609df294be37be6e17895d07d72ca3a34ec5078b
brainchemist2005 <zackariaebouargan@gmail.com>
2023-05-19 16:26:02 -0400

    Ajout de la méthode "lecture" dans la classe GestionJson et modifications dans la classe Main.

--- GestionJson.java
+++ GestionJson.java
@@ -1,2 +1,53 @@
+import java.io.*;
+
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+import org.apache.commons.beanutils.BeanUtils;
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.*;
+
 public class GestionJson {
+    public static void lecture(String json)
+    {
+        System.out.println();
+
+        int matricule = 0, type, distance = 0, overtime = 0,hours = 0;
+        String horaire_min, horaire_max;
+        String code = null, date;
+
+        JSONObject employee;
+        
+        employee = JSONObject.fromObject(json);
+
+        matricule = employee.getInt("matricule_employe");
+        type = employee.getInt("type_employe");
+        horaire_min = employee.getString("taux_horaire_min");
+        horaire_max = employee.getString("taux_horaire_max");
+        JSONArray interventions = employee.getJSONArray("interventions");
+        for(int i=0; i<interventions.size() ; i++)
+        {
+            JSONObject intervention = interventions.getJSONObject(i);
+
+            try
+            {
+                code = intervention.getString("code_client");
+            }
+
+            catch(Exception e)
+            {
+                code = intervention.getString("code_projet");
+            }
+            distance = intervention.getInt("distance_deplacement");
+            overtime = intervention.getInt("overtime");
+            hours = intervention.getInt("nombre_heures");
+            date = intervention.optString("date_intervention", intervention.optString("date_affectation"));
+        }
+
+
+        System.out.println(matricule + " " + type + " ");
+        for(int i=0; i< interventions.size() ;i++)
+        {
+            System.out.println(distance +" " +overtime +" "+ hours +" "+ code);
+        }
+    }
 }
--- Main.java
+++ Main.java
@@ -1,6 +1,34 @@
+import net.sf.json.JSON;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+
+import java.io.*;
 public class Main {
     public static void main(String[] args)
     {
-        System.out.println("Hello Word");
+
+        String arugument = "test.json";
+        String json="",buffer;
+
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(arugument))) {
+
+            while ((buffer =reader.readLine()) != null)
+            {
+                if(buffer != null)
+                    json += buffer;
+                json += "\n";
+
+            }
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+        GestionJson.lecture(json);
     }
+
+
+
 }
