7f42b25964f1fef4889dd7da4369a14599523661
ricky-the-god <nguyen.ricky_vinh-an@courrier.uqam.ca>
2023-06-27 16:56:16 -0400

    Deplacement de methodes de la class main a la class CalculEmplye

--- Source/CalculEmploye.java
+++ Source/CalculEmploye.java
@@ -1,8 +1,11 @@
 package Source;
 
 import java.util.Arrays;
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
 
 public class CalculEmploye {
+
     /**
      * Vérifie si un entier `j` est présent dans le tableau `nbrs`.
      *
@@ -10,7 +13,7 @@ public class CalculEmploye {
      * @param j    L'entier à rechercher.
      * @return `true` si l'entier `j` n'est pas présent dans le tableau `nbrs`, `false` sinon.
      */
-    public static boolean verification(int[] nbrs, int j) {
+    public static boolean verification(int[] nbrs, int j) { //l'argument "j" refere au code client dans le fichier JSON
         for (int i = 0; i < nbrs.length; i++) {
             if (nbrs[i] == j)
                 return false;
@@ -18,31 +21,82 @@ public class CalculEmploye {
         return true;
     }
 
+    public static boolean validerFormatDate(String dateStr) {
+        try {
+            LocalDate.parse(dateStr);
+            return true;
+        } catch (DateTimeParseException e) {
+            return false;
+        }
+    }
 
+    public static double checkDistance(double nbr) throws JsonException
+    {
+        if (nbr < 0.0 || nbr > 100.0) {
+            throw new JsonException("Distance deplacement invalide");
+        }
+        return nbr;
+    }
 
-    /**
-     * Effectue le traitement des données et génère les résultats correspondants.
-     *
-     * @param data       Les données d'entrée sous forme de tableau à deux dimensions.
-     * @param argument2  Le fichier sortie.json a remplir.
-     */
-    public static void executer(String[][] data, String argument2) {
+    public static double checkOvertime(double nbr) throws JsonException
+    {
+        if (nbr < 0.0 || nbr > 4.0) {
+            throw new JsonException("Overtime invalide");
+        }
+
+        return nbr;
+    }
+
+    public static double checkNombreHeures(double nbr) throws JsonException
+    {
+        if (nbr < 0.0  || nbr > 8.0) {
+            throw new JsonException("Nombre d'heures invalide");
+        }
+
+        return nbr;
+    }
+
+    public static double checkerTaux(String[][] data,int i) throws JsonException
+    {
+        double taux_horaire;
+        try {
+            taux_horaire = Double.parseDouble(data[i][0].substring(0, 5));
+        }
+        catch (Exception e)
+        {
+            taux_horaire = Double.parseDouble(data[i][0].replace(",", ".").replace("$", ""));// Sa c'est pour gerer le virgule
+        }
+
+        if(taux_horaire < 0)
+            throw new JsonException("Taux horaire invalide");
+
+        return taux_horaire;
+    }
+
+    public static void checkerTypeEmploye(int type) throws JsonException {
+        if (type < 0 || type > 2) {
+            throw new JsonException("Type d'employé invalide");
+        }
+    }
+
+    public static void validerNombreInterventions(int nombre) throws JsonException {
+        if (nombre > 10) {
+            throw new JsonException("Le nombre d’interventions est supérieur à 10.");
+        }
+    }
+
+    //TODO: rendre la methode executer plus courte en creant des sous methodes
+
+    public static void executer(String[][] data, String argument2) throws JsonException {
         int[] nbrs = new int[30];
         Arrays.fill(nbrs, -1);
         int status, matricule_employe, type_employe, itterations = Integer.parseInt(data[data.length - 1][0]);
         int[] distance_deplacement = new int[Integer.parseInt(data[data.length - 1][0])];
         int[] overtime = new int[Integer.parseInt(data[data.length - 1][0])];
         int[] nombre_heures = new int[Integer.parseInt(data[data.length - 1][0])];
-        double taux_horaire_min, taux_horaire_max, montantDeplacement = 0, coutVariable, montantRegulier = 0,
-                montantHeureSupplementaire = 0;
         String[] code = new String[Integer.parseInt(data[data.length - 1][0])];
-        double[] EtatParClient = new double[code.length];
 
-        // Récupération des données d'entrée
-        matricule_employe = Integer.parseInt(data[0][0]);
-        type_employe = Integer.parseInt(data[1][0]);
-        taux_horaire_min = Double.parseDouble(data[2][0].substring(0, 5));
-        taux_horaire_max = Double.parseDouble(data[3][0].substring(0, 5));
+
 
         // Traitement des données
         for(int i=0,j=4; j< data.length-1;j++, i++)
@@ -51,18 +105,66 @@ public class CalculEmploye {
             if(status != -1 && status != -2)
             {
                 code[i] = data[j][0];
+                try
+                {
+                    checkDistance(Integer.parseInt(data[j][1]));
+                    checkOvertime(Integer.parseInt(data[j][2]));
+                    checkNombreHeures(Integer.parseInt(data[j][3]));
+                }
+                catch (JsonException e)
+                {
+                    System.out.println(e.getMessage());
+                    System.exit(1);
+                }
+
                 distance_deplacement[i] = Integer.parseInt(data[j][1]) +Integer.parseInt( data[status][1]);
                 overtime[i] = Integer.parseInt(data[j][2])  + Integer.parseInt(data[status][2]);
                 nombre_heures[i] = Integer.parseInt(data[j][3]) + Integer.parseInt(data[status][3]);// i am not sure of this
-                //itterations--;
             }
             else if(status == -1)
             {
                 code[i] = data[j][0];
+                try
+                {
+                    checkDistance(Integer.parseInt(data[j][1]));
+                    checkOvertime(Integer.parseInt(data[j][2]));
+                    checkNombreHeures(Integer.parseInt(data[j][3]));
+                }
+                catch (JsonException e)
+                {
+                    System.out.println(e.getMessage());
+                    System.exit(1);
+                }
                 distance_deplacement[i] = Integer.parseInt(data[j][1]);
                 overtime[i] = Integer.parseInt(data[j][2]);
                 nombre_heures[i] = Integer.parseInt(data[j][3]);
             }
+        }
+
+        double taux_horaire_min = 0, taux_horaire_max = 0, montantDeplacement = 0, coutVariable, montantRegulier = 0,
+                montantHeureSupplementaire = 0;
+        double[] EtatParClient = new double[code.length];
+
+        // Récupération des données d'entrée
+        matricule_employe = Integer.parseInt(data[0][0]);
+        type_employe = Integer.parseInt(data[1][0]);
+
+        try {
+            checkerTaux(data, 2);
+        }
+        catch (JsonException e)
+        {
+            System.out.println(e.getMessage());
+            System.exit(1);
+        }
+
+        try {
+            checkerTaux(data, 3);
+        }
+        catch (JsonException e)
+        {
+            System.out.println(e.getMessage());
+            System.exit(1);
         }
 
 
@@ -72,19 +174,16 @@ public class CalculEmploye {
                 EtatParClient[i] = calculerEtatParClient(type_employe,nombre_heures[i],taux_horaire_min,taux_horaire_max,distance_deplacement[i],overtime[i],montantRegulier);
         }
 
-        // Calcul de l'état par client pour chaque itération
         for (int i = 0; i < itterations; i++) {
             EtatParClient[i] = calculerEtatParClient(type_employe, nombre_heures[i],
                     taux_horaire_min, taux_horaire_max, distance_deplacement[i], overtime[i], montantRegulier);
         }
 
-        // Calcul de l'état de compte total et des coûts
         double etatCompteTotal = calculerEtatCompteTotal(EtatParClient);
         coutVariable = calculerCoutVariable(etatCompteTotal);
         double coutFixe = calculerCoutFixe(etatCompteTotal);
 
-        // Écriture des résultats dans un fichier JSON
-        GestionJson.ecrireFichierSortieJson(matricule_employe, arrondirMontant(etatCompteTotal), arrondirMontant(coutFixe),
+        GestionJson.formattageFichierSortieJson(matricule_employe, arrondirMontant(etatCompteTotal), arrondirMontant(coutFixe),
                 arrondirMontant(coutVariable),code,EtatParClient,itterations, argument2, nbrs);
     }
 
@@ -115,7 +214,13 @@ public class CalculEmploye {
             }
         }
         return -1; // Aucune occurrence précédente n'a été trouvée
+
+        //TODO: enlever le return -1 et la remplacer par un try catch statement
     }
+
+
+
+    //TODO: ajouter une methode qui check l'occurence
 
 
 
@@ -130,25 +235,19 @@ public class CalculEmploye {
      */
     public static double calculerMontantRegulier(int typeEmploye, double nombreHeures,
                                                  double tauxHoraireMin, double tauxHoraireMax) {
-        if (tauxHoraireMin < 0 || tauxHoraireMax < 0 || nombreHeures < 0){
-            throw new IllegalArgumentException("Le taux horaire ou le nombre d'heure n'est pas valide.");
-        }
+
 
         double tauxHoraire = 0;
 
 
-        //Selectionner type d'employe
         if (typeEmploye == 0){
             tauxHoraire = tauxHoraireMin;
         } else if (typeEmploye == 1) {
             tauxHoraire = (tauxHoraireMin + tauxHoraireMax)/2;
         } else if (typeEmploye == 2) {
             tauxHoraire = tauxHoraireMax;
-        } else {
-            throw new IllegalArgumentException("La valeur n'est pas valide. La valeur ne peut pas etre plus petite que 0 et plus grande que 2.");
         }
 
-        // Calculer le montant régulier en multipliant le taux horaire par le nombre d'heures
         double montantRegulier = tauxHoraire * nombreHeures;
 
         return montantRegulier;
@@ -163,22 +262,19 @@ public class CalculEmploye {
      * @return
      */
     public static double calculerMontantDeplacement(int typeEmploye, double distanceDeplacement,
-                                                    double montantRegulier){
-        if (distanceDeplacement < 0 || montantRegulier < 0){
-            throw new IllegalArgumentException("La valeur n'est pas valide.");
-        }
+                                                    double montantRegulier) throws JsonException {
+        double CalculemontantDeplacement = 0;
 
-        double montantDeplacement = 0;
+        double montantDeplacement = 0.0;
 
-        // Calculer le montant de déplacement en fonction du type d'employé
-        if (typeEmploye == 0){
-            montantDeplacement = 200 - (distanceDeplacement * (0.05 * montantRegulier)) ;
+        if (typeEmploye == 0) {
+            CalculemontantDeplacement = 200 - (distanceDeplacement * (0.05 * montantRegulier));
         } else if (typeEmploye == 1) {
-            montantDeplacement = 200 - (distanceDeplacement * (0.10 * montantRegulier));
+            CalculemontantDeplacement = 200 - (distanceDeplacement * (0.10 * montantRegulier));
         } else if (typeEmploye == 2) {
-            montantDeplacement = 200 - (distanceDeplacement * (0.15 * montantRegulier));
+            CalculemontantDeplacement = 200 - (distanceDeplacement * (0.15 * montantRegulier));
         } else {
-            throw new IllegalArgumentException("La valeur n'est pas valide.");
+            montantDeplacement = checkDistance(CalculemontantDeplacement);
         }
 
         return montantDeplacement;
@@ -194,22 +290,15 @@ public class CalculEmploye {
     public static double calculerMontantHeuresSupplementaires(int typeEmploye, double overtime,double nombre_heures) {
         double montantHeuresSupplementaires = 0.0;
 
-        if (overtime < 0 || nombre_heures < 0){
-            throw new IllegalArgumentException("Overtime ou nombre d'heure n'est pas valide.");
-        }
-
         if (typeEmploye == 0) {
-            // Superviseur : Pas de montant pour les heures supplémentaires
             montantHeuresSupplementaires = 0.0;
         } else if (typeEmploye == 1) {
-            // Permanent
             if (nombre_heures > 4 && nombre_heures <= 8) {
                 montantHeuresSupplementaires = 50.0 * overtime;
             } else if (nombre_heures > 8) {
                 montantHeuresSupplementaires = 100.0 * overtime;
             }
         } else if (typeEmploye == 2) {
-            // Contractuel
             if (overtime <= 4)
             {
                 montantHeuresSupplementaires = 75.0 * overtime;
@@ -223,7 +312,6 @@ public class CalculEmploye {
             throw new IllegalArgumentException("Type d'employer n'est pas valide.");
         }
 
-        // Limiter le montant des heures supplémentaires à 1500.00
         montantHeuresSupplementaires = Math.min(montantHeuresSupplementaires, 1500.0);
 
         return montantHeuresSupplementaires;
@@ -242,30 +330,37 @@ public class CalculEmploye {
     }
 
     public static double arrondirMontant(double montant) {
-        // On multiplie par 20 pour déplacer la décimale au deuxième chiffre après la virgule
-        return Math.ceil(montant * 20) / 20;
+        double arrondi = Math.ceil(montant * 20) / 20; // Arrondi à 2 décimales
+        double difference = arrondi - Math.floor(arrondi); // Partie décimale
+        if (difference < 0.05) {
+            return Math.floor(arrondi * 20) / 20; // Arrondi au multiple inférieur de 0,05
+        } else {
+            return Math.ceil(arrondi * 20) / 20; // Arrondi au multiple supérieur de 0,05
+        }
     }
+
 
     public static double calculerEtatParClient(int typeEmploye, double nombreHeures,
                                                double tauxHoraireMin, double tauxHoraireMax,
-                                               double distanceDeplacement, double overtime, double montantregulier) {
+                                               double distanceDeplacement, double overtime, double montantregulier) throws JsonException {
         double montantTotal = 0.0;
 
-        // Calcul du montant des heures travaillées
         double montantHeuresTravaillees = calculerMontantRegulier(typeEmploye, nombreHeures,
                 tauxHoraireMin, tauxHoraireMax);
 
         montantTotal += montantHeuresTravaillees;
 
-        // Calcul du montant des heures supplémentaires
         double montantHeuresSupplementaires = calculerMontantHeuresSupplementaires(typeEmploye, overtime, nombreHeures);
         montantTotal += montantHeuresSupplementaires;
 
-        // Calcul du montant de déplacement
         double montantDeplacement = calculerMontantDeplacement(typeEmploye, distanceDeplacement, montantregulier);
         montantTotal += montantDeplacement;
 
         montantTotal = arrondirMontant(montantTotal);
+
+        if (montantTotal < 0) {
+            throw new JsonException ("Le montant total ne peut pas être négatif");
+        }
 
         return montantTotal;
     }
@@ -288,25 +383,18 @@ public class CalculEmploye {
     public static double calculerCoutFixe(double etatCompteTotal) {
         double coutFixe;
 
-        // Calcul du coût fixe en fonction de l'État de compte total
-
-        // Si l'état de compte total est supérieur ou égal à 1000.0
         if (etatCompteTotal >= 1000.0) {
             coutFixe = etatCompteTotal * 0.012;
         }
-        // Si l'état de compte total est supérieur ou égal à 500.0 mais inférieur à 1000.0
         else if (etatCompteTotal >= 500.0) {
             coutFixe = etatCompteTotal * 0.008;
         }
-        // Si l'état de compte total est inférieur à 500.0
         else {
             coutFixe = etatCompteTotal * 0.004;
         }
 
-        // Arrondir le coût fixe au 5 sous supérieur
         coutFixe = arrondirMontant(coutFixe);
 
         return coutFixe;
     }
-
 }
