4cbcc36ed97d59bbf13d313150ebba80c35ae5d8
AlibayPatelHoussen <gg491974@ens.uqam.ca>
2023-06-30 00:47:11 -0400

    j'ai corrigé la methode preparationJson de la classe GestionJson

--- Source/GestionJson.java
+++ Source/GestionJson.java
@@ -78,7 +78,7 @@ public class GestionJson {
 
     private static JSONArray preparationJson(String[] code, double[] etat_par_client, int j, int[] nbrs, JSONObject employee, CalculEmploye calculEmploye, JSONArray clients, JSONObject client) {
         for(int i = 0; i < j; i++) {
-            if(GestionProgramme.verification(nbrs, i)) {
+            if(GestionProgramme.verificationCodeClient(nbrs, i)) {
                 client.accumulate("code_client", code[i]);
                 client.accumulate("etat_par_client", String.format("%.2f$", etat_par_client[i]));
                 clients.add(client);
