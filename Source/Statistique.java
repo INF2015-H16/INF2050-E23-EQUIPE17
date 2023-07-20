b09ea80eb9637ee1f28fb362e44e28a8ecee70fd
KennyIdo <steveken758@gmail.com>
2023-07-20 11:54:42 -0400

    J'ai crée les constantes de la classe Statistiques.

--- Source/Statistique.java
+++ Source/Statistique.java
@@ -1,4 +1,10 @@
 package Source;
 
 public class Statistique {
+    final static int TYPE_EMPLOYE_0 = 0;
+    final static int TYPE_EMPLOYE_1 = 1;
+    final static int TYPE_EMPLOYE_2 = 2;
+    final static int ETAT_PAR_CLIENT_1000 = 1000;
+    final static int ETAT_PAR_CLIENT_10000 = 10000;
+
 }
