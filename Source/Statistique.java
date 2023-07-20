20f7e1e2995946a679ee41c92d47abfff59acadc
KennyIdo <steveken758@gmail.com>
2023-07-20 16:38:23 -0400

    J'ai crée la méthode calculerHeureMaxPourIntervention.

--- Source/Statistique.java
+++ Source/Statistique.java
@@ -1,5 +1,10 @@
 package Source;
 
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+
+import java.io.IOException;
+
 public class Statistique {
     final static int TYPE_EMPLOYE_0 = 0;
     final static int TYPE_EMPLOYE_1 = 1;
@@ -7,4 +12,20 @@ public class Statistique {
     final static int ETAT_PAR_CLIENT_1000 = 1000;
     final static int ETAT_PAR_CLIENT_10000 = 10000;
 
+    public static int calculeHeureMaxIntervention(String jsonObject, JSONArray tableauInterventions,
+                                                  String arg3) throws IOException {
+
+        JSONObject employe = JSONObject.fromObject(jsonObject);
+        tableauInterventions = employe.getJSONArray("interventions");
+        int heureMax = 0;
+        for (Object interventionObj : tableauInterventions) {
+            JSONObject interventionJson = (JSONObject) interventionObj;
+            int nombreHeures = interventionJson.getInt("nombre_heures");
+            heureMax = Math.max(heureMax, nombreHeures);
+        }
+        GestionProgramme.ajouterMessage("Le nombre d'heures maximal pour une intervention est de" +" "+ heureMax
+                ,arg3);
+
+        return heureMax;
+    }
 }
