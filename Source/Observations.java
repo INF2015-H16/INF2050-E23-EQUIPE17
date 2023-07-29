6a0f71251dee2326bf71afd2106036ca1d73214e
brainchemist2005 <zackariaebouargan@gmail.com>
2023-07-29 00:53:46 -0400

    J'ai reglé les conflits

--- Source/Observations.java
+++ Source/Observations.java
@@ -7,32 +7,30 @@ public class Observations {
 
     public static void observationTaux(double tauxHoraireMax, double tauxHoraireMin, JSONArray observations) {
 
-        if(tauxHoraireMax > 2 * tauxHoraireMin) {
+        if (tauxHoraireMax > 2 * tauxHoraireMin) {
             observations.add("Le taux horaire maximum ne peut pas dépasser deux fois le taux horaire minimum.");
         }
     }
 
-    public static void employeeObservation(String code, int overtime,int distance_deplacement,JSONArray observations) {
+    public static void employeeObservation(String code, int overtime, int distance_deplacement, JSONArray observations) {
 
-        if(distance_deplacement > 50)
+        if (distance_deplacement > 50)
             observations.add("Le client " + code + " a une distance de deplacement plus que 50 km");
     }
 
     static void observationDates(String[][] attributsJson, JSONArray observations) {
 
-        String date1,date2,codeClient;
-        int mois1,annee1,annee2,mois2,monthsApart;
+        String date1, date2, codeClient;
+        int mois1, annee1, annee2, mois2, monthsApart;
 
-        int i=4;
+        int i = 4;
 
-        while(attributsJson[i][4] != null){
+        while (attributsJson[i][4] != null) {
             codeClient = attributsJson[i][0];
             date1 = attributsJson[i][4];
 
-            for(int j=i+1; j<attributsJson.length-1 ; j++)
-            {
-                if(attributsJson[j][0].equals(codeClient))
-                {
+            for (int j = i + 1; j < attributsJson.length - 1; j++) {
+                if (attributsJson[j][0].equals(codeClient)) {
                     date2 = attributsJson[j][4];
                     mois1 = Integer.parseInt(date1.substring(5, 7));
                     annee1 = Integer.parseInt(date1.substring(0, 4));
@@ -40,16 +38,16 @@ public class Observations {
                     mois2 = Integer.parseInt(date2.substring(5, 7));
                     annee2 = Integer.parseInt(date2.substring(0, 4));
 
-                    if(annee2 > annee1 && mois2 > mois1)
+                    if (annee2 > annee1 && mois2 > mois1)
                         monthsApart = (annee2 - annee1) * 12 + (mois2 - mois1);
-                    else if(annee2 > annee1 && mois2 < mois1)
+                    else if (annee2 > annee1 && mois2 < mois1)
                         monthsApart = (annee2 - annee1) * 12 + (mois1 - mois2);
-                    else if(annee2 < annee1 && mois2 > mois1)
+                    else if (annee2 < annee1 && mois2 > mois1)
                         monthsApart = (annee1 - annee2) * 12 + (mois2 - mois1);
                     else
                         monthsApart = (annee1 - annee2) * 12 + (mois1 - mois2);
 
-                    if(monthsApart >= 6)
+                    if (monthsApart >= 6)
                         observations.add("L’écart maximal entre les dates d’intervention (date_intervention) du client "
                                 + codeClient + " d’un même employé doit être de moins de 6 mois.");
                 }
@@ -59,17 +57,9 @@ public class Observations {
         }
     }
 
-<<<<<<< HEAD
     static JSONObject employeeObservation(JSONObject employee, double cout_variable, double etat_compte, double cout_fixe, JSONArray observations){
-        if(cout_variable > 3000){
-=======
-    static JSONObject employeeObservation(JSONObject employee, double cout_variable, double etat_compte,
-                                          double cout_fixe, JSONArray observations){
         if(cout_variable > 3000)
->>>>>>> 6bf2274445cdc4935163cdb230f6aec30dde5e74
             observations.add("Le cout variable payable nécessite des ajustements");
-            System.out.println(cout_variable);
-       }
         if(etat_compte > 30000)
             observations.add("L’état de compte total ne doit pas dépasser 30000.00 $.");
         if(cout_fixe > 1500)
@@ -77,9 +67,10 @@ public class Observations {
         return employee;
     }
 
-    static void etatClientObservation(JSONArray observation, JSONObject employee, double etat_par_client, String code) {
+        static void etatClientObservation (JSONArray observation, JSONObject employee,double etat_par_client, String
+        code){
+            if (etat_par_client > 15000)
+                observation.add("L’état par client du client " + code + " est trop dispendieuse.");
+        }
 
-        if(etat_par_client > 15000)
-            observation.add("L’état par client du client " + code + " est trop dispendieuse.");
-    }
 }
