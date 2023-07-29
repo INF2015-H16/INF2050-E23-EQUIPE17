25ff9697f2826b89935abf75a7b093f91ed3cfba
KennyIdo <steveken758@gmail.com>
2023-07-28 22:20:19 -0400

    J'ai corrigé l'indentation du code dans la classe GestionJson.

--- Source/GestionJson.java
+++ Source/GestionJson.java
@@ -10,8 +10,10 @@ import java.util.Locale;
 public class GestionJson {
 
     public static String convertirMajusculesEnMinuscules(String valeur) {
+
         if (valeur != null) {
             valeur = valeur.toLowerCase();
+
             if (valeur.contains(" ")) {
                 valeur = valeur.replace(" ", "");
             }
@@ -27,7 +29,9 @@ public class GestionJson {
      * @param interventions
      * @return Un tableau à deux dimensions contenant les données extraites du JSON.
      */
-    public static String[][] lireFichierEntreeJson(String json, String cheminJson, JSONArray observations, JSONArray interventions) throws JsonException, IOException{
+    public static String[][] lireFichierEntreeJson(String json, String cheminJson, JSONArray observations,
+                                                   JSONArray interventions) throws JsonException, IOException{
+
         JSONObject employee = JSONObject.fromObject(json);
         interventions = employee.getJSONArray("interventions");
         String[][] attributsJson = new String[5+interventions.size()][5]; //La methode calculInterventions permet de savoir combien on va resever d'espace pour les interventions
@@ -40,13 +44,16 @@ public class GestionJson {
     }
 
     private static void observationDates(String[][] attributsJson, JSONArray observations) {
+
         String date1,date2,codeClient;
         int mois1,annee1,annee2,mois2,monthsApart;
 
         int i=4;
+
         while(attributsJson[i][4] != null){
             codeClient = attributsJson[i][0];
             date1 = attributsJson[i][4];
+
             for(int j=i+1; j<attributsJson.length-1 ; j++)
             {
                 if(attributsJson[j][0].equals(codeClient))
@@ -76,12 +83,16 @@ public class GestionJson {
         }
     }
 
-    private static void recuperationInfos(JSONObject employee, JSONArray interventions, String[][] attributsJson, String cheminJson) throws JsonException, IOException {
+    private static void recuperationInfos(JSONObject employee, JSONArray interventions, String[][] attributsJson,
+                                          String cheminJson) throws JsonException, IOException {
+
         recuperationInfo(attributsJson, employee);
         recuperationInfo(attributsJson, interventions, cheminJson);
     }
 
-    private static void recuperationInfo(String[][] attributsJson, JSONArray interventions, String cheminJson) throws JsonException, IOException {
+    private static void recuperationInfo(String[][] attributsJson, JSONArray interventions, String cheminJson)
+            throws JsonException, IOException {
+
         try {
             for (int compteurInterventions = 0; compteurInterventions < interventions.size(); compteurInterventions++) {
                 reccuperationAttributs(attributsJson, interventions, compteurInterventions);
@@ -93,7 +104,9 @@ public class GestionJson {
         JsonException.validationDate(attributsJson);
     }
 
-    private static void reccuperationAttributs(String[][] attributsJson, JSONArray interventions, int compteurInterventions) {
+    private static void reccuperationAttributs(String[][] attributsJson, JSONArray interventions,
+                                               int compteurInterventions) {
+
         JSONObject intervention = interventions.getJSONObject(compteurInterventions);
         attributsJson[4 + compteurInterventions][0] = intervention.optString("code_client").toUpperCase(); // Utilisation de optString au lieu de getString
         attributsJson[4 + compteurInterventions][1] = intervention.getString("distance_deplacement");
@@ -103,6 +116,7 @@ public class GestionJson {
     }
 
     private static void recuperationInfo(String[][] attributsJson, JSONObject employee) {
+
         attributsJson[0][0] = employee.getString("matricule_employe");
         attributsJson[1][0] = employee.getString("type_employe");
         attributsJson[2][0] = employee.getString("taux_horaire_min");
@@ -110,7 +124,11 @@ public class GestionJson {
     }
 
 
-    public static void formattageFichierSortieJson(int matricule_employe, double etat_compte, double cout_fixe, double cout_variable, String[] code, double[] etat_par_client, int j, String arg, int[] nbrs, JSONArray observation, JSONObject statistique, String option) {
+    public static void formattageFichierSortieJson(int matricule_employe, double etat_compte, double cout_fixe,
+                                                   double cout_variable, String[] code, double[] etat_par_client, int j,
+                                                   String arg, int[] nbrs, JSONArray observation,
+                                                   JSONObject statistique, String option) {
+
         JSONObject employee = new JSONObject();
         employee = employeeInfo(matricule_employe, etat_compte, cout_fixe, cout_variable, employee,observation);
         JSONArray clients = new JSONArray();
@@ -121,31 +139,44 @@ public class GestionJson {
         Statistiques.calculerOccurrencesEtatParClient(employee,statistique);
         Statistiques.calculerTotalInterventions(employee,statistique);
         Statistiques.ecrireStatisques(statistique,"Statistique.json",option);
+
         ecritureFichierSortieJson(arg,employee);
     }
 
-    public static JSONObject employeeInfo(int matricule_employe, double etat_compte, double cout_fixe, double cout_variable, JSONObject employee,JSONArray observations) {
+    public static JSONObject employeeInfo(int matricule_employe, double etat_compte, double cout_fixe,
+                                          double cout_variable, JSONObject employee,JSONArray observations) {
+
         employee.accumulate("matricule_employe", matricule_employe);
         employee.accumulate("etat_compte", String.format(Locale.CANADA,"%.2f$", etat_compte));
         employee.accumulate("cout_fixe", String.format(Locale.CANADA,"%.2f$", cout_fixe));
         employee.accumulate("cout_variable", String.format(Locale.CANADA,"%.2f$", cout_variable));
+
         employeeObservation(employee,cout_variable,etat_compte,cout_fixe,observations);
         return employee;
     }
 
-    private static JSONObject employeeObservation(JSONObject employee, double cout_variable, double etat_compte, double cout_fixe, JSONArray observations) {
+    private static JSONObject employeeObservation(JSONObject employee, double cout_variable, double etat_compte,
+                                                  double cout_fixe, JSONArray observations) {
+
         if (cout_variable > 3000)
             observations.add("Le cout variable payable nécessite des ajustements");
+
         if (etat_compte > 30000)
             observations.add("L’état de compte total ne doit pas dépasser 30000.00 $.");
+
         if (cout_fixe > 1500)
             observations.add("Le cout fixe payable ne doit pas dépasser 1500.00 $.");
+
         return employee;
     }
 
 
-    public static JSONArray preparationJson(String[] code, double[] etat_par_client, int j, int[] nbrs, JSONArray clients, JSONObject client, JSONObject employee, JSONArray observation) {
+    public static JSONArray preparationJson(String[] code, double[] etat_par_client, int j, int[] nbrs,
+                                            JSONArray clients, JSONObject client, JSONObject employee,
+                                            JSONArray observation) {
+
         for(int i = 0; i < j; i++) {
+
             if(GestionProgramme.verificationCodeClient(nbrs, i) && code[i] != null) {
                 client.accumulate("code_client", code[i]);
                 client.accumulate("etat_par_client", String.format(Locale.CANADA,"%.2f$", etat_par_client[i]));
@@ -157,13 +188,16 @@ public class GestionJson {
         return clients;
     }
 
-    private static void etatClientObservation(JSONArray observation, JSONObject employee, double etat_par_client,String code) {
+    private static void etatClientObservation(JSONArray observation, JSONObject employee, double etat_par_client,
+                                              String code) {
+
         if(etat_par_client > 15000)
             observation.add("L’état par client du client " + code + " est trop dispendieuse.");
     }
 
 
     private static void ecritureFichierSortieJson(String arg,JSONObject employee) {
+
         try {
             FileUtils.writeStringToFile(new File(arg), employee.toString(2), "UTF-8");// le 2 dans tostring sert a ecrire le json d'une facon indente
         } catch (IOException e) {
