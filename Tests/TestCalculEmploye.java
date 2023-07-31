1cd6e9a6391cb5b9f728303b96ae8029a285dae7
KennyIdo <steveken758@gmail.com>
2023-07-31 16:47:49 -0400

    J'ai renommé des variables dans TestCalculEmploye.

--- Tests/TestCalculEmploye.java
+++ Tests/TestCalculEmploye.java
@@ -11,25 +11,25 @@ public class TestCalculEmploye {
     @Test
     public void testCalculerEtatCompteTotal() {
         double[] etatsParClient = {100.0, 200.0, 300.0};
-        double expectedEtatCompteTotal = 733.77 + 100.0 + 200.0 + 300.0;
-        double actualEtatCompteTotal = CalculEmploye.calculerEtatCompteTotal(etatsParClient);
-        Assertions.assertEquals(expectedEtatCompteTotal, actualEtatCompteTotal, 0.001);
+        double etatCompteTotalAttendu = 733.77 + 100.0 + 200.0 + 300.0;
+        double etatCompteTotalActuel = CalculEmploye.calculerEtatCompteTotal(etatsParClient);
+        Assertions.assertEquals(etatCompteTotalAttendu, etatCompteTotalActuel, 0.001);
     }
 
     @Test
     public void testCalculerCoutVariable() {
         double etatCompteTotal = 1000.0;
-        double expectedCoutVariable = (2.5 / 100) * etatCompteTotal;
-        double actualCoutVariable = CalculEmploye.calculerCoutVariable(etatCompteTotal);
-        Assertions.assertEquals(expectedCoutVariable, actualCoutVariable, 0.001);
+        double coutVariableAttendu = (2.5 / 100) * etatCompteTotal;
+        double coutVariableActuel = CalculEmploye.calculerCoutVariable(etatCompteTotal);
+        Assertions.assertEquals(coutVariableAttendu, coutVariableActuel, 0.001);
     }
 
     @Test
     public void testCalculerCoutFixe() {
         double etatCompteTotal = 2000.0;
-        double expectedCoutFixe = etatCompteTotal * (1.2 / 100);
-        double actualCoutFixe = CalculEmploye.calculerCoutFixe(etatCompteTotal);
-        Assertions.assertEquals(expectedCoutFixe, actualCoutFixe, 0.001);
+        double coutFixeAttendu = etatCompteTotal * (1.2 / 100);
+        double coutFixeActuel = CalculEmploye.calculerCoutFixe(etatCompteTotal);
+        Assertions.assertEquals(coutFixeAttendu, coutFixeActuel, 0.001);
     }
 
     @Test
@@ -47,10 +47,10 @@ public class TestCalculEmploye {
         montantDeplacement = (200 - (70 * (0.10 * montantRegulier)));;
         montantHeuresSupp = (nombreHeures > 40) ? (nombreHeures - 40) * 100.0 * overtime : 0;
 
-        double expectedEtatParClient =  montantRegulier + montantDeplacement + montantHeuresSupp;
-        double actualEtatParClient = CalculEmploye.calculerEtatParClient(typeEmploye, nombreHeures, tauxHoraireMin, tauxHoraireMax, distanceDeplacement, overtime);
-        System.out.print(actualEtatParClient);
-        Assertions.assertEquals(expectedEtatParClient, actualEtatParClient, 0.001);
+        double etatParClientAttendu =  montantRegulier + montantDeplacement + montantHeuresSupp;
+        double etatParClientActuel = CalculEmploye.calculerEtatParClient(typeEmploye, nombreHeures, tauxHoraireMin, tauxHoraireMax, distanceDeplacement, overtime);
+        System.out.print(etatParClientActuel);
+        Assertions.assertEquals(etatParClientAttendu, etatParClientActuel, 0.001);
 
     }
 
@@ -68,9 +68,9 @@ public class TestCalculEmploye {
         montantDeplacement = (200 - (70 * (0.10 * montantRegulier)));;
         montantHeuresSupp = (nombreHeures > 40) ? (nombreHeures - 40) * 100.0 * overtime : 0;
 
-        double expectedEtatParClient =  montantRegulier + montantDeplacement + montantHeuresSupp;
-        double actualEtatParClient = CalculEmploye.calculerEtatParClient(typeEmploye, nombreHeures, tauxHoraireMin, tauxHoraireMax, distanceDeplacement, overtime);
-        Assertions.assertEquals(expectedEtatParClient, actualEtatParClient, 0.001);
+        double etatParClientAttendu =  montantRegulier + montantDeplacement + montantHeuresSupp;
+        double etatParClientActuel = CalculEmploye.calculerEtatParClient(typeEmploye, nombreHeures, tauxHoraireMin, tauxHoraireMax, distanceDeplacement, overtime);
+        Assertions.assertEquals(etatParClientAttendu, etatParClientActuel, 0.001);
     }
 
     @Test
@@ -80,9 +80,9 @@ public class TestCalculEmploye {
         double tauxHoraireMin = 20.0;
         double tauxHoraireMax = 30.0;
 
-        double expectedMontantRegulier = tauxHoraireMax * nombreHeures;
-        double actualMontantRegulier = CalculEmploye.calculerMontantRegulier(typeEmploye, nombreHeures, tauxHoraireMin, tauxHoraireMax);
-        Assertions.assertEquals(expectedMontantRegulier, actualMontantRegulier, 0.001);
+        double montantRegulierAttendu = tauxHoraireMax * nombreHeures;
+        double montantRegulierActuel = CalculEmploye.calculerMontantRegulier(typeEmploye, nombreHeures, tauxHoraireMin, tauxHoraireMax);
+        Assertions.assertEquals(montantRegulierAttendu, montantRegulierActuel, 0.001);
     }
 
     @Test
@@ -91,36 +91,36 @@ public class TestCalculEmploye {
         double distanceDeplacement = 80.0;
         double montantRegulier = 1000.0;
 
-        double expectedMontantDeplacement = 200 - (distanceDeplacement * (0.10 * montantRegulier));
-        double actualMontantDeplacement = CalculEmploye.calculerMontantDeplacement(typeEmploye, distanceDeplacement, montantRegulier);
-        Assertions.assertEquals(expectedMontantDeplacement, actualMontantDeplacement, 0.001);
+        double montantDeplacementAttendu = 200 - (distanceDeplacement * (0.10 * montantRegulier));
+        double montantDeplacementActuel = CalculEmploye.calculerMontantDeplacement(typeEmploye, distanceDeplacement, montantRegulier);
+        Assertions.assertEquals(montantDeplacementAttendu, montantDeplacementActuel, 0.001);
     }
 
     @Test
     public void testCalculerMontantHeuresSupplementairesType1() {
         double overtime = 5.0;
 
-        double expectedMontantHeuresSupplementaires = 50.0 * overtime;
-        double actualMontantHeuresSupplementaires = CalculEmploye.calculerMontantHeuresSupplementairesType1(overtime);
-        Assertions.assertEquals(expectedMontantHeuresSupplementaires, actualMontantHeuresSupplementaires, 0.001);
+        double montantHrsSupAttendu = 50.0 * overtime;
+        double montantHrsSupActuel = CalculEmploye.calculerMontantHeuresSupplementairesType1(overtime);
+        Assertions.assertEquals(montantHrsSupAttendu, montantHrsSupActuel, 0.001);
     }
 
     @Test
     public void testCalculerMontantHeuresSupplementairesType2() {
         double overtime = 3.5;
 
-        double expectedMontantHeuresSupplementaires = 75.0 * overtime;
-        double actualMontantHeuresSupplementaires = CalculEmploye.calculerMontantHeuresSupplementairesType2(overtime);
-        Assertions.assertEquals(expectedMontantHeuresSupplementaires, actualMontantHeuresSupplementaires, 0.001);
+        double montantHrsSupAttendu = 75.0 * overtime;
+        double montantHrsSupActuel = CalculEmploye.calculerMontantHeuresSupplementairesType2(overtime);
+        Assertions.assertEquals(montantHrsSupAttendu, montantHrsSupActuel, 0.001);
     }
 
     @Test
     public void testArrondirMontant() {
         double montant = 456.78;
 
-        double expectedArrondi = 456.80;
-        double actualArrondi = CalculEmploye.arrondirMontant(montant);
-        Assertions.assertEquals(expectedArrondi, actualArrondi, 0.001);
+        double arrondissementAttendu = 456.80;
+        double arrondissementActuel = CalculEmploye.arrondirMontant(montant);
+        Assertions.assertEquals(arrondissementAttendu, arrondissementActuel, 0.001);
     }
 
 }
