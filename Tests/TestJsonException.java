db1d39bb3839480e1ba2a90fc2310fadaa7aa0ad
ricky-the-god <nguyen.ricky_vinh-an@courrier.uqam.ca>
2023-07-31 22:36:46 -0400

    Correction TesTJsonException

--- Tests/TestJsonException.java
+++ Tests/TestJsonException.java
@@ -4,10 +4,6 @@ import Source.JsonException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
-import java.io.File;
-import static org.junit.jupiter.api.Assertions.*;
-
 public class TestJsonException {
     @Test
     public void testValiderDistance_Valid() {
